eb4cf0cc8d48c3ecfcdfbda4478a5ee320b928df
tiandiyihao0 <654969283@qq.com>
2019-11-02 16:20:53 +0800

    !!

--- src/main/java/com/service/HelloService.java
+++ src/main/java/com/service/HelloService.java
@@ -3,6 +3,6 @@ package com.service;
 public class HelloService {
 	public static void main(String[] args) {
 		System.out.println("hello service");
-		System.out.println("git2 edit");
+		System.out.println("git2 edit!!");
 	}
 }
